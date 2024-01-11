9cf005c5da34fce075fdef8ce6c0556f547172c1
Aman Kumar Kushwaha <amansom155@gmail.com>
2024-01-11 20:50:14 +0530

    Model_trainer py file configured and add evaluate model function in utils.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 
 from src.components import data_transformation
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 from src.exception import CustomException
 from src.logger import logging
 
@@ -60,4 +61,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
+        train_data, test_data
+    )
+    trainer = ModelTrainer()
+    print(trainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr))
